9dc8de0eeaa7dad4b6f2e79267ab1658a06a342e
myCodeSpace <get110000@163.com>
2019-07-22 20:52:56 +0800

    Create FrontCell.java
    创建元素元接口

---
+++ src/main/java/com/xier/data/FrontCell.java
@@ -0,0 +1,21 @@
+
+
+/**
+ * cell单元的元模型
+ * 
+ * <h3>什么是元模型</h3> 元模型<meta-model>是一个元素能力的描述。<br>
+ * 
+ * @author xier
+ * @see 
+ * @see 
+ * @see 
+ */
+public interface FrontCell {
+	
+	/**
+	 * 填充页面label
+	 * @author lvhui5 2017年11月7日 下午3:09:27
+	 * @param linkageData 实现填充的逻辑
+	 */
+	public void setOtherAttr(LinkHandler linkHander);
+}
