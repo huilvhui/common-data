99ea472d8a14b3e10e2d0e654525dd49d66796d6
myCodeSpace <get110000@163.com>
2019-07-24 11:34:01 +0800

    Create PersistConstruct.java

---
+++ src/main/java/com/xier/dynamic/data/constructor/PersistConstruct.java
@@ -0,0 +1,14 @@
+package com.xier.dynamic.data.constructor;
+
+import com.xier.dynamic.data.block.PersistBlock;
+
+
+public interface PersistConstruct {
+	
+	/**
+	 * 构造一个持久化块
+	 * @author lvhui5 2017年7月21日 下午1:57:50
+	 */
+	public void construct(PersistBlock persistBlock);
+	
+}
