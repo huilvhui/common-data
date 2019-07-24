a79eef585d10b408fedcced68c62b739003dbcab
myCodeSpace <get110000@163.com>
2019-07-24 11:20:57 +0800

    Create EnumDataType.java

---
+++ src/main/java/com/xier/dynamic/constant/EnumDataType.java
@@ -0,0 +1,77 @@
+package com.xier.dynamic.constant;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import jef.database.dialect.ColumnType;
+
+
+/**
+ * <p>
+ * 数据类型比较枚举
+ * 数据模型定义动态属性的数据类型
+ * </p>
+ * @author lvhui5 2017年8月11日 下午2:07:33
+ * @version V1.0
+ */
+public enum EnumDataType {
+	
+	BLOB("blob", "长字符型"," ",new ColumnType.Varchar(2048).notNull().defaultIs(CharacterConstants.NEW_COLUMN_SPACE)),
+	VARCHAR("varchar", "字符型"," ",new ColumnType.Varchar(128).notNull().defaultIs(CharacterConstants.NEW_COLUMN_SPACE)), 
+	INT("int", "整型","0",new ColumnType.Int(8).notNull().defaultIs(0)), 
+	FLOAT("float", "浮点型","0",new ColumnType.Double(8, 2).notNull().defaultIs(0));
+
+	private String name;
+	private String value;
+	private String defaultValue;
+	private ColumnType columnType;
+	/**
+	 * 全局索引池
+	 */
+	private static Map<String, EnumDataType> pool = new HashMap<String, EnumDataType>();
+	static {
+		for (EnumDataType et : EnumDataType.values()) {
+			pool.put(et.name, et);
+		}
+	}
+	
+	/**
+	 * 根据内容索引
+	 * @param value
+	 * @return
+	 */
+	public static EnumDataType indexByValue(String name) {
+		return pool.get(name);
+	}
+	
+	private EnumDataType(String name, String value,String defaultValue,ColumnType columnType) {
+		this.name = name;
+		this.value = value;
+		this.defaultValue = defaultValue;
+		this.columnType = columnType;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	
+    public String getDefaultValue() {
+    	return defaultValue;
+    }
+    
+    public ColumnType getColumnType() {
+    	return columnType;
+    }
+
+
+
+
+
+
+	
+}
