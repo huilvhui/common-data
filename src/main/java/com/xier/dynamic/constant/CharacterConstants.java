f5afbd32c8313a14dfbdad33c72c2afc61af4d11
myCodeSpace <get110000@163.com>
2019-07-24 11:18:38 +0800

    Create CharacterConstants.java

---
+++ src/main/java/com/xier/dynamic/constant/CharacterConstants.java
@@ -0,0 +1,18 @@
+package com.xier.dynamic.constant;
+
+/**
+ * <p>
+ * 特殊字符常量类
+ * </p>
+ * @author lvhui5 2017年5月18日 上午11:14:10
+ * @version V1.0
+ */
+public class CharacterConstants {
+	
+	public static final String NEW_LINE_N = "\n";
+	public static final String NEW_LINE_SEP = "&";// 上报文件分隔符
+	public static final String NEW_LINE_WRAP = "\r\n";// 上报文件换行符(Windows下记事本的格式，Linux是完全支持)
+	public static final String ROOT_PATH = "/";
+	public static final String NEW_COLUMN_SEPARATOR = "\\|";
+	public static final String NEW_COLUMN_SPACE = " ";
+}
